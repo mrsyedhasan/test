de7218e19aec2a780799ea70bb3c9e964c3aa7d2
syednychasan@gmail.com <syedhasan@Syeds-MacBook-Pro.local>
2020-06-09 01:17:50 -0400

    selenium test to verify Google title

--- src/test/java/testdemo/Selenium_Test.java
+++ src/test/java/testdemo/Selenium_Test.java
@@ -1,4 +1,33 @@
 package testdemo;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.swing.*;
+
+import static org.junit.Assert.assertEquals;
+
 public class Selenium_Test {
+WebDriver driver;
+
+    @Before
+    public void openbrowser(){
+    System.setProperty("webdriver.chrome.driver","/Users/syedhasan/IdeaProjects/test_method/src/main/resources/chromedriver");
+    driver = new ChromeDriver();
+    }
+    @After
+    public void closeAll(){
+    driver.quit();
+    }
+    @Test
+    public void openThePageAndCheckTittle(){
+        String expectedTitle = "Google";
+        driver.get("https://www.google.com/");
+        String title = driver.getTitle();
+        System.out.println(title);
+        assertEquals(expectedTitle, title);
+    }
 }
